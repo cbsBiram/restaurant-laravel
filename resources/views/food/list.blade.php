e9337aa20a187d390f03362579f67a4d3777e54f
Biram Diouf <ibrahimabiram@gmail.com>
2020-11-26 15:36:28 +0000

    Initial commit

---
+++ resources/views/food/list.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        @foreach ($categories as $category)
+            
+        <div class="col-md-12">
+            <h2 style="color: blue">{{$category->name}}</h2>
+            <div class="jumbotron">
+                <div class="row"> 
+                    @foreach (App\Food::where('category_id', $category->id)->get() as $food)
+                        
+                    <div class="col-md-3">
+
+                        <img src="{{asset('images')}}/{{$food->image}}" width="200" height="155">
+                        <p class="text-center">{{$food->name}}
+                            <span>${{$food->price}}</span>
+                        </p>
+                        <p class="text-center">
+                            <a type="button" class="btn btn-outline-danger" href="{{route('food.view', [$food->id])}}">
+                                View
+                            </a>
+                        </p>
+                    </div>
+
+                    @endforeach
+                </div>
+            </div>
+
+        @endforeach
+        </div> 
+    </div>
+</div>
+    
+@endsection
